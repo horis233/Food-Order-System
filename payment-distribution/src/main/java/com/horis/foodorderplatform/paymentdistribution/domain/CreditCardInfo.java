5abafa92de3ebbbaa43f103cd3cd1bbafa521656
horis233 <horis233@gmail.com>
2018-08-24 14:10:18 -0400

    developing payment distribution on port 8003 which implement the Eureka and Hystrix

--- payment-distribution/src/main/java/com/horis/foodorderplatform/paymentdistribution/domain/CreditCardInfo.java
+++ payment-distribution/src/main/java/com/horis/foodorderplatform/paymentdistribution/domain/CreditCardInfo.java
@@ -1,4 +1,15 @@
 package com.horis.foodorderplatform.paymentdistribution.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
 public class CreditCardInfo {
+    private String firstName;
+    private String lastName;
+    private int expiredMonth;
+    private int expiredYear;
+    private int securityCode;
 }
+
